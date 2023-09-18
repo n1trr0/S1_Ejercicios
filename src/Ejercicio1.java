d5f389e2ad4310d37e0d6987f883862d6e6ac7e7
Raul <raulgarcicespedes@gmail.com>
2023-09-18 18:25:52 +0200

    subida ejercicio 1 y calculadora .java

---
+++ src/Ejercicio1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculadora calculadora = new Calculadora();
+
+        System.out.println("Introduce el primer número:");
+        double numero1 = scanner.nextDouble();
+        calculadora.setX(numero1);
+
+        System.out.println("Introduce el segundo número:");
+        double numero2 = scanner.nextDouble();
+        calculadora.setY(numero2);
+
+        double resultado = calculadora.suma();
+        System.out.println("La suma es: " + resultado);
+
+        scanner.close();
+    }
+}
